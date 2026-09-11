9f8177062e65259bb9e3df33bf01d451795f4a2e
Mahitha <mahithanagari@gmail.com>
2026-09-11 10:42:41 +0530

    commit

---
+++ JavaPro/src/com/pro/Pencil.java
@@ -0,0 +1,17 @@
+package com.pro;
+
+public class Pencil {
+
+	public static void main(String[] args) {
+		int money = 100;
+		int pencilCost = 7;
+		int pencils = money / pencilCost;
+		int balance = money %  pencilCost;
+		System.out.println("No.of pencils:" + pencils );
+		System.out.println("balnace:" + balance );
+
+		
+		
+	}
+
+}
